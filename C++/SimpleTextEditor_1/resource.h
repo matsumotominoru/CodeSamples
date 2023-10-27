085961eaf599f2a611931ef8d711a0a98636e141
matsumotominoru <matsumoto.minoru.su@alumni.tsukuba.ac.jp>
2023-10-27 15:14:09 +0900

    Add NekoRikaiWin4 Sample
    NekoRikaiWin4 - Chapter13 - memo01
    Simple Text Editor

---
+++ C++/SimpleTextEditor_1/resource.h
@@ -0,0 +1,8 @@
+// resource.h
+
+#define IDM_END                         101
+#define IDM_SAVEAS                      102
+#define IDM_SAVE                        103
+#define IDM_NEW                         104
+#define IDM_OPEN                        105
+
